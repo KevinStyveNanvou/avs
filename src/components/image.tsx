58963cbbf2962a5a4a3a2588086eb90df42c4c64
Kevin Nanvou <nanvoukevinstyve@gmail.com>
2026-03-23 14:02:42 +0100

    tgjhkl

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Image() {
   return (
     <div>
-        <section className="relative h-[50vh]">
+        <section className="relative h-[20vh] lg:h-[40vh]">
   {/* Background */}
   <div
     className="absolute inset-0 bg-cover bg-center bg-fixed"
